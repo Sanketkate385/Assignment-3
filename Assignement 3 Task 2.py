03546c8690d85c7c2debd13e5964fb7b452595b3
Sanketkate385 <sanket.kate385@gmail.com>
2025-06-09 23:51:46 +0530

    Add files via upload

---
+++ Assignement 3 Task 2.py
@@ -0,0 +1,17 @@
+# Task 2:
+    
+from math import *
+
+def Operations(n):
+
+    print("Logirathmic Value of", n, "is", log(n))
+
+    print("Square root of", n, "is", sqrt(n))
+
+    print("Sine Value of", n, "in radians is", sin(n))
+    
+
+
+n = float(input("Enter the number: "))
+
+Operations(n)
